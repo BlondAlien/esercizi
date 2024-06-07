b3ff7293aff0eb136ab7904c9993050461d43bf4
Bogdan Gaspar <b.gspr16@yahoo.com>
2024-06-07 15:08:36 +0200

    aggiunti consegna esercizi Esercizi Variabili e Costanti Javascript

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function sayHello(){
-    alert("Ciao mondo");
-}
+
+// Esercizio 1
+// Dichiarare una variabile chiamata "numero" e assegnarle il valore 5. Stampare il valore della variabile sulla console.
+
+
+
+// Esercizio 2
+// Dichiarare una costante chiamata "PI" e assegnarle il valore di 3.14. Stampare il valore della costante sulla console.
+
+
+
+// Esercizio 3
+// Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". 
+// Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
+
+
+// Esercizio 4
+// Dichiarare una variabile chiamata "saluto" e assegnarle il valore "Ciao, come stai?". Stampare il valore della variabile sulla console.
+
+
+
+// 🍰 Esercizio 5
+// Dichiarare una variabile chiamata "vero" e assegnarle il valore true. Stampare il valore della variabile sulla console.
+
+
+// 🍰 Esercizio 6 (opzionale)
+// Dichiarare una variabile chiamata "numeri" e assegnarle un array contenente i numeri da 1 a 5. Stampare l'array sulla console.
+
+
+// 🍰 Esercizio 7 (opzionale)
+// Dichiarare una variabile chiamata "persona" e assegnarle un oggetto con le seguenti proprietà: "nome" (con valore "Mario") e "età" (con valore 30). Stampare l'oggetto sulla console.
