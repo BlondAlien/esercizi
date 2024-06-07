12818474288117f951be3c7332d9266f0cc46fd7
sara <saratraversa09@gmail.com>
2024-06-07 15:21:59 +0200

    eseguiti

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 
-// Esercizio 1
+// Esercizio 1 
 // Dichiarare una variabile chiamata "numero" e assegnarle il valore 5. Stampare il valore della variabile sulla console.
-
+let numero = 5;
+console.log(numero);
 
 
 // Esercizio 2
 // Dichiarare una costante chiamata "PI" e assegnarle il valore di 3.14. Stampare il valore della costante sulla console.
-
+const PI = 3.14;
+console.log(PI);
 
 
 // Esercizio 3
 // Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". 
 // Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
-
+let nome = "Marco";
+nome = "Luca";
+console.log(nome);
 
 // Esercizio 4
 // Dichiarare una variabile chiamata "saluto" e assegnarle il valore "Ciao, come stai?". Stampare il valore della variabile sulla console.
+let saluto = "ciao come stai?";
+console.log(saluto);
 
 
 
 // 🍰 Esercizio 5
 // Dichiarare una variabile chiamata "vero" e assegnarle il valore true. Stampare il valore della variabile sulla console.
-
+let vero = "true";
+console.log(vero);
 
 // 🍰 Esercizio 6 (opzionale)
 // Dichiarare una variabile chiamata "numeri" e assegnarle un array contenente i numeri da 1 a 5. Stampare l'array sulla console.
-
+let numeri = [1, 2, 3, 4, 5];
+console.log(numeri);
 
 // 🍰 Esercizio 7 (opzionale)
 // Dichiarare una variabile chiamata "persona" e assegnarle un oggetto con le seguenti proprietà: "nome" (con valore "Mario") e "età" (con valore 30). Stampare l'oggetto sulla console.
+let persona = {
+    nome: "Mario",
+    età: 30,
+}
+console.log(persona);
