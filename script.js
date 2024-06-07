81b0487b13e0bc9881945743b8b2d904e7cb5482
Bogdan Gaspar <b.gspr16@yahoo.com>
2024-06-07 16:09:06 +0200

    nuove consegne

--- script.js
+++ script.js
@@ -170,3 +170,40 @@ function Esercizi_Lavorare_con_Numeri_Javascript() {
     let numerover = parseInt(numeroqua);
 
 }
+
+
+function Esercizi_Operatori_di_Comparazione_Javascript() {
+
+// Esercizio 1
+// Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è maggiore del secondo.
+
+
+//  Esercizio 2
+// Stampa sulla console il risultato del confronto tra due stringhe per verificare se la prima stringa viene prima della seconda in ordine alfabetico.
+
+
+//  Esercizio 3
+// Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono uguali.
+
+
+//  Esercizio 4
+// Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono uguali sia in valore che in tipo.
+
+
+//  Esercizio 5
+// Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono diversi.
+
+
+//  Esercizio 6
+// Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono diversi sia in valore che in tipo.
+
+
+//  Esercizio 7
+// Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è minore-uguale del secondo.
+
+
+//  Esercizio 8
+// Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è diverso dal secondo.
+
+
+}
