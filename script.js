f9144991c6cc8c9af11e2e7059ff59662424dc66
Bogdan Gaspar <b.gspr16@yahoo.com>
2024-06-08 10:02:13 +0200

    consegna operatori logici javascript

--- script.js
+++ script.js
@@ -207,3 +207,30 @@ function Esercizi_Operatori_di_Comparazione_Javascript() {
 
 
 }
+
+
+function Esercizi_Operatori_Logici_Javascript(){
+
+// Esercizio 1
+// Stampa sulla console il risultato della valutazione di due condizioni a scelta utilizzando l'operatore AND.
+
+
+// Esercizio 2
+// Stampa sulla console il risultato della valutazione di due condizioni a scelta utilizzando l'operatore OR.
+
+
+// Esercizio 3
+// Stampa sulla console il risultato della negazione di una condizione a scelta utilizzando l'operatore NOT.
+
+
+// Esercizio 4
+// Date due costanti valutare se valore1 è maggiore di 0 e valore2 è minore di 20, oppure se valore1 è uguale a 5.
+
+
+// Esercizio 5
+// Date due stringhe valutare se stringa1 non è vuota e stringa2 non è la parola "treno".
+
+
+// Esercizio 6
+// Dati tre numeri valutare se: valore1 è maggiore di zero oppure valore2 è minore di venti. Queste condizioni devono verificarsi insieme a valore3 compreso tra 10 e 20.
+}
